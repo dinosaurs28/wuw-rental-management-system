9a78bc2d75dc233376dcb84585c25a9ea8a7d3ea
manish076 <manishku073@gmail.com>
2025-12-08 16:58:53 +0530

    [FEAT] KycUploaded check Middleware Added

---
+++ apps/backend/src/middlewares/kycCheck.middlewares.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request,Response } from "express"
+import { StatusCode } from "../types/statusCode"
+import { prisma } from "@repo/database/client"
+export const kycCheck=async(req:Request,res:Response,next:NextFunction)=>{
+    try{
+        const public_Id=req.public_Id
+        if(!public_Id){
+            return res.status(StatusCode.BAD_REQUEST).json({
+                message:"The Public Id Is Missing"
+            })
+        }
+        const isKycUpload=await prisma.customerKyc.findUnique({
+            where:{
+                publicId:public_Id
+            }
+        })
+        if(!isKycUpload){
+            return res.status(StatusCode.FORBIDDEN).json({
+                message:"Please ensure you upload a valid, clear image of your Driver's License."
+            })
+        }
+        return next();
+    }catch(e:any){
+        console.log("Internal Error While Checking Kyc Has been Upload",e)
+        return res.status(StatusCode.INTERNAL_SERVER_ERROR).json({
+            message:"Internal Error While Checking Kyc Has been Upload"
+        })
+    }
+}
