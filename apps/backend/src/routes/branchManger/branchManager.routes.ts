2b26bedcce6fc8232bb5496c4f6704f4edb9bb51
manish076 <manishku073@gmail.com>
2025-12-31 17:29:55 +0530

    [FEAT] Get Return Route BM

--- apps/backend/src/routes/branchManger/branchManager.routes.ts
+++ apps/backend/src/routes/branchManger/branchManager.routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { Login } from "../../controller/branchManager/login.controller";
 import { ManagerCheck } from "../../middlewares/managerCheck.middlewares";
 import { GetRevenueStats } from "../../controller/branchManager/revenue.controller";
-import { GetActiveBookings } from "../../controller/branchManager/bookings.controller";
+import { GetActiveBookings, GetPendingApprovals } from "../../controller/branchManager/bookings.controller";
 
 const router:Router=Router()
 
 router.post("/auth/login", Login)
 router.get("/dashboard/revenue", ManagerCheck, GetRevenueStats)
 router.get("/dashboard/bookings/active", ManagerCheck, GetActiveBookings)
+router.get("/dashboard/bookings/pending", ManagerCheck, GetPendingApprovals)
 
 export default router
